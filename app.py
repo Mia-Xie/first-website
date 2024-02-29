b183dacfaf305f45e8c9c0da85797c83ba857854
miaxie <miaxie95@outlook.com>
2024-02-29 01:46:09 -0500

    added html and css with bootstrap css more

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
+PROD = [
+    {'id':1,
+     'title': 'necklace with heart',
+     'price': 49.99,
+     'in stock':'y'
+    },
+    {'id': 2,
+     'title': 'necklace 2',
+     'price': 49.99,
+     'in stock':'y'
+     },
+    {'id': 3,
+     'title': 'necklace 3',
+     'price': 49.99,
+    'in stock':'n'
+     }
+]
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html',
+                           products=PROD,
+                           company_name='AuroraOrnaments')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
